554bf22502f648dc6b1793e7061e720569d6cce6
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-03-23 20:09:31 -0300

    Update ContatoModel.js

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const ContatoSchema = new mongoose.Schema({
-    title: {
-        type: "string",
-        required: true,
-    },
-    description: {
-        type: "string",
-        required: true,
-    }
-})
+    name: { type: String, required: true, },
+    surname: { type: String, required: false, default: "" },
+    email: { type: String, required: false, default: "" },
+    tel: { type: String, required: false, default: "" },
+    dateCreate: { type: Date, default: Date.now },
+});
 
 const ContatoModel = mongoose.model("Contato", ContatoSchema);
 
@@ -21,5 +19,31 @@ class Contato {
     }
 }
 
+Contato.prototype.register = function () {
+    this.valid();
+};
+
+Contato.prototype.valid = function () {
+    this.cleanUp();
+    // Validando
+    // Email tem que ser válido
+    if (!validator.isEmail(this.body.email)) { this.errors.push('Email inválido.'); };
+}
+
+Contato.prototype.cleanUp = function () {
+    for (const key in this.body) {
+        if (typeof this.body[key] !== 'string') {
+            this.body[key] = '';
+        }
+    }
+
+    this.body = {
+        name: this.body.name,
+        surname: this.body.surname,
+        email: this.body.email,
+        tel: this.body.tel,
+    };
+
+}
 
 module.exports = Contato;
