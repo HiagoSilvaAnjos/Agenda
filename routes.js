31db853608e117533c2f54037d4749d6d1754199
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-03-25 19:17:41 -0300

    Cadastrando o usuário

--- routes.js
+++ routes.js
@@ -18,5 +18,6 @@ route.get("/login/logout", loginController.logout);
 // Rotas para contato
 route.get("/contato/", loginRequired, contatoController.index);
 route.post("/contato/register", loginRequired, contatoController.register);
+route.get("/contato/index/:id", loginRequired, contatoController.editIndex);
 
 module.exports = route;
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -11,26 +11,36 @@ const ContatoSchema = new mongoose.Schema({
 
 const ContatoModel = mongoose.model("Contato", ContatoSchema);
 
-class Contato {
-    constructor(body) {
-        this.body = body;
-        this.erros = [];
-        this.contato = null;
-    }
+function Contato(body) {
+    this.body = body;
+    this.errors = [];
+    this.contato = null;
 }
 
-Contato.prototype.register = function () {
-    this.valid();
-};
+Contato.getIdContato = async function (id) {
+    if (typeof id !== 'string') return;
+    const user = await ContatoModel.findById(id);
+    return user;
+}
 
-Contato.prototype.valid = function () {
+Contato.prototype.register = async function () {
+    this.valid();
+
+    if (this.errors.length > 0) return;
+
+    this.contato = await ContatoModel.create(this.body);
+}
+
+Contato.prototype.valid = async function () {
     this.cleanUp();
     // Validando
     // Email tem que ser válido
-    if (!validator.isEmail(this.body.email)) { this.errors.push('Email inválido.'); };
+    if (this.body.email && !validator.isEmail(this.body.email)) { this.errors.push('Email inválido.'); };
+    if (!this.body.name) this.errors.push('Nome é um campo obrigatório.');
+    if (!this.body.email && !this.body.tel) this.errors.push('Pelo menos um contato precisa ser enviado: email ou telefone');
 }
 
-Contato.prototype.cleanUp = function () {
+Contato.prototype.cleanUp = async function () {
     for (const key in this.body) {
         if (typeof this.body[key] !== 'string') {
             this.body[key] = '';
@@ -43,7 +53,54 @@ Contato.prototype.cleanUp = function () {
         email: this.body.email,
         tel: this.body.tel,
     };
-
 }
+
+// class Contato {
+//     constructor(body) {
+//         this.body = body;
+//         this.errors = [];
+//         this.contato = null;
+//     }
+
+//     async register() {
+//         this.valid();
+
+//         if (this.errors.length > 0) return;
+
+//         this.contato = await ContatoModel.create(this.body);
+//     }
+
+//     async getIdContato(id) {
+//         if (typeof id !== 'string') return;
+//         const user = await ContatoModel.findById(id);
+//         return user;
+//     }
+
+//     valid() {
+//         this.cleanUp();
+//         // Validando
+//         // Email tem que ser válido
+//         if (this.body.email && !validator.isEmail(this.body.email)) { this.errors.push('Email inválido.'); };
+//         if (!this.body.name) this.errors.push('Nome é um campo obrigatório.');
+//         if (!this.body.email && !this.body.tel) this.errors.push('Pelo menos um contato precisa ser enviado: email ou telefone');
+//     }
+
+//     cleanUp() {
+//         for (const key in this.body) {
+//             if (typeof this.body[key] !== 'string') {
+//                 this.body[key] = '';
+//             }
+//         }
+
+//         this.body = {
+//             name: this.body.name,
+//             surname: this.body.surname,
+//             email: this.body.email,
+//             tel: this.body.tel,
+//         };
+
+//     }
+// }
+
 
 module.exports = Contato;
